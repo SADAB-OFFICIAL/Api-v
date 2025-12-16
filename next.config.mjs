7b2a8bf31d9e808df391e94dceb58fd88db2319e
SADAB ALI <sadabgithub@gmail.com>
2025-12-16 23:39:07 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "movies4u.nexus" },
+      { protocol: "https", hostname: "catimages.org" },
+      { protocol: "https", hostname: "img.youtube.com" },
+    ],
+  },
+};
+
+export default nextConfig;
